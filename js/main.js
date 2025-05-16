a7a60368081b809a355c1a1bc1fd52e039773f31
Cristian <cris.cuevashuerta@gmail.com>
2025-05-16 16:58:29 -0400

    update front secciones

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ form.addEventListener('submit', async (e) => {
             body: JSON.stringify(datos)
         });
 
-        const respuesta = await res.json();
+        const respuesta = await res.json(); 
         alert(respuesta.message);
         form.reset();
         } catch (err) {
@@ -25,19 +25,3 @@ form.addEventListener('submit', async (e) => {
         } 
 });
 
-let currentSlide = 0;
-const slides = document.querySelectorAll(".slide");
-
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.classList.remove("active");
-    if (i === index) slide.classList.add("active");
-  });
-}
-
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
-}
-
-setInterval(nextSlide, 5000); // Cambia cada 5 segundos
